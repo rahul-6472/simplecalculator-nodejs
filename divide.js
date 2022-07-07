36fbed83e0774115c7a86d42a24087e77ce10f98
rahul-6472 <rahulpandey0663@gmail.com>
2022-07-07 18:07:26 +0530

    first commit

---
+++ divide.js
@@ -0,0 +1,5 @@
+const divide = (a,b) =>{ 
+    return Math.floor(a/b)
+ }
+
+ module.exports = divide
---
+++ main.js
@@ -0,0 +1,9 @@
+const add = require("./add")
+const multiply = require("./multiply")
+const divide = require("./divide")
+const subtract = require("./subtract")
+
+console.log(add(3,4))
+console.log(multiply(4,5))
+console.log(subtract(10,7))
+console.log(divide(10,2))
